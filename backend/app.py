5a669696a005fafce4ac9c90d3c9991a9a76ec83
E <mcarzaa@gmail.com>
2024-11-24 11:28:45 -0500

    Moved mock data to api folder

--- backend/app.py
+++ backend/app.py
@@ -4,7 +4,7 @@ from backend.api.send_stream import router as send_stream_router
 from backend.api.get_live_stats import router as get_live_stats_router
 from backend.api.get_historical_stats import router as get_historical_stats_router
 from backend.api.kafka_topics import router as kafka_topics_router
-from tests.streaming.stream import router as test_streaming_router
+from backend.tests.streaming.stream import router as test_streaming_router
 
 app = FastAPI()
 
